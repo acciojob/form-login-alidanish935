b0eeff9da254d77537160ef184d8a1f866002533
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-07-17 18:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const name = document.getElementByQuerySelectors('input');
+	const name = document.getElementByQuerySelectorAll('input');
 	const fn = name[0];
 	const ln = name[1];
 	alert(fn,ln)
